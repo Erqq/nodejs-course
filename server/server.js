a55e0673d37d020c5d41ea076bd74be1d872b390
Eerik Timonen <eerik.timonen94@gmail.com>
2018-05-02 10:18:35 +0300

    middleware and add code to app.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ app.use(bodyparser.json());
 
 let lions = [];
 let id = 0;
+app.param("id", (req, res, next, id) => {
+  let lion = _.find(lions, { id: id });
+  if (lion) {
+    req.lion = lions;
+    next();
+  } else {
+    res.send();
+  }
+});
 
 app.get("/", (req, res) => {
   res.sendfile(__dirname + "/client/index.html", function(err) {
@@ -29,7 +38,19 @@ app.get("/lions/:id", (req, res) => {
   let lion = _.find(lions, { id: req.params.id });
   res.json(lion || {});
 });
-
+app.put("/lions/:id", (req, res) => {
+  let update = req.body;
+  if (update.id) {
+    delete update.id;
+  }
+  var lion = _.findIndex(lions, { id: req.params.id });
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var updatedlion = _.assign(lions[lion], update);
+    res.json(updatedlion);
+  }
+});
 app.post("/lions", (req, res) => {
   let lion = req.body;
   id++;
@@ -37,7 +58,22 @@ app.post("/lions", (req, res) => {
   lions.push(lion);
   res.json(lion);
 });
+app.delete("/lions/:id", (req, res) => {
+  let lion = _.findIndex(lions, { id: req.params.id });
 
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var deletedLion = lions[lion];
+    lions.splice(lion, 1);
+    res.json(deletedLion);
+  }
+});
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).send(err);
+  }
+});
 app.listen(port, function() {
   console.log("listening on localhost:", port);
 });
